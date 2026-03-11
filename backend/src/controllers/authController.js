80d778dd67c1cc33d4da203e707b6c637dbda8d1
abdielpaulino <abdielpaulino8@gmail.com>
2026-03-10 21:28:11 -0300

    initial project structure

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,7 @@
+export const registerUser = (req, res) => {
+  res.json({ message: "User successfully registered." });
+};
+
+export const loginUser = (req, res) => {
+  res.json({ message: "User successfully logged in." });
+};
---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import apiRoutes from "./routes/apiRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use(apiRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
